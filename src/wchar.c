e91cb5bf389253b4f9d5e8d86caf74e030912003
Benson Lee <blee@e1z2r8p27.42.us.org>
2017-07-11 19:59:26 -0700

    wide char start

---
+++ src/wchar.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wchar.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/11 15:23:28 by blee              #+#    #+#             */
+/*   Updated: 2017/07/11 19:58:50 by blee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/* need to include stddef.h or wchar.h? */
+
+char	*one_byte_str(wchar_t wc)
+{
+}
+
+char	*two_byte_str(wchar_t wc)
+{
+}
+
+char	*three_byte_str(wchar_t wc)
+{
+}
+
+char	*four_byte_str(wchar_t wc)
+{
+}
+
+char	*wchar_to_str(wchar_t wc)
+{
+	/* MB_CUR_MAX */
+}
