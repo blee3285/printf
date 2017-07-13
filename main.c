012a812d05b183268aafd560616dd419c5619a3d
Benson Lee <blee@e1z2r4p9.42.us.org>
2017-07-12 18:59:14 -0700

    starting on wide char

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/17 20:31:18 by blee              #+#    #+#             */
-/*   Updated: 2017/07/11 14:23:26 by blee             ###   ########.fr       */
+/*   Updated: 2017/07/12 16:49:18 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@
 
 int		main(void)
 {
-	char	ptr;
+	unsigned char	ptr;
 	int		*formats;
 	int		i;
-	char	c;
+	unsigned char	c;
+	wchar_t wc;
 	char	*str;
 
 	ptr = 'a';
@@ -50,12 +51,124 @@ int		main(void)
 	ft_printf("HEX: %X\nUNSIGNED INT: %u\n", 255, 2148000000);
 	ft_printf("POINTER: %p\n", &ptr);
 	*/
-	//str = ft_strdup("     123");
+	//str = ft_strdup("     123");i
 	//printf("OLD STR: |%s|\n", str);
 	//flag_id(&str, 3, '-');
 	//printf("NEW STR: |%s|\n", str);
 	
-	i = ft_printf("|%*.*d|\n", 5, -5, 50);
+	//i = ft_printf("|%*.*d|\n", 5, -5, 50);
+
+	wc = L'¢';
+	//ptr = (unsigned char)((wc >> 6) | 0xC0);
+	//c = (unsigned char)((wc & 0x3F) | 0x80);
 	
+	ft_putnbr(wc);
+	ft_putstr("\n");
+	ft_putstr(ft_itoa_base(wc, 2, 0));
+	ft_putstr("\nFirst byte:\n");
+	ptr = (unsigned char) (wc >> 6);
+	ft_putstr(ft_itoa_base(ptr, 2, 0));
+	ft_putchar('\n');
+	ptr = (ptr | 0xC0);
+	ft_putstr(ft_itoa_base(ptr, 2, 0));
+	ft_putchar('\n');
+	ft_putstr("Second byte:\n");
+	c = (unsigned char) (wc & 0x3F);
+	ft_putstr(ft_itoa_base(c, 2, 0));
+	ft_putchar('\n');
+	c = (c | 0x80);
+	ft_putstr(ft_itoa_base(c, 2, 0));
+	ft_putchar('\n');
+
+	ft_putchar(ptr);
+	ft_putchar(c);
+
+	/*
+	char	*str = "hellodf";
+
+	setlocale(LC_ALL, "");
+	printf("printf: |%&&jhdfhs|\n", 10, 23);
+	ft_printf("printf: |%&&jhdfhs|\n", 10, 23);
+	printf("%d\n", printf("%hhljtz 4.8%04", 1));
+	ft_printf("%d\n", ft_printf("%hhljtz 4.8%04", 1));
+	printf("printf: |%05=acs%05=|\n", 555);
+	ft_printf("printf: |%05=acs%05=|\n", 555);
+	printf("printf: |hello%.3llld|\n", SHRT_MAX);
+	ft_printf("printf: |hello%.3llld|\n", SHRT_MAX);
+	printf("printf: |%jdgb|\n", 525);
+	ft_printf("printf: |%jdgb|\n", 525);
+	printf("printf: |%-+8.9d|%ld|%-+8.9hhhhd|\n", 2345, LONG_MIN, 2345);
+	ft_printf("printf: |%-+8.9d|%ld|%-+8.9hhhhd|\n", 2345, LONG_MIN, 2345);
+	printf("|%-06%-05=%-5#d|\n", 555);
+	ft_printf("|%-06%-05=%-5#d|\n", 555);
+	printf("|%s%9.5S%ls|\n", "hello", L"漢字", L"漢字");
+	ft_printf("|%s%9.5S%ls|\n", "hello", L"漢字", L"漢字");
+	printf("|%c%C%lc|\n", 'h', L'漢', L'漢');
+	ft_printf("|%c%C%lc|\n", 'h', L'漢', L'漢');
+	printf("printf: |%#1.5x|\n", LONG_MAX);
+	ft_printf("printf: |%#1.5x|\n", LONG_MAX);
+	printf("printf: |%#lo|%#ho|%#hho|\n", LONG_MAX, LONG_MAX, LONG_MAX);
+	ft_printf("printf: |%#lo|%#ho|%#hho|\n", LONG_MAX, LONG_MAX, LONG_MAX);
+	printf("printf: |%#1.5x|\n", LONG_MIN);
+	ft_printf("printf: |%#1.5x|\n", LONG_MIN);
+	printf("printf: |%#1.5u|\n", LONG_MIN);
+	ft_printf("printf: |%#1.5u|\n", LONG_MIN);
+	printf("printf: |%#1.5U|\n", LONG_MAX);
+	ft_printf("printf: |%#1.5U|\n", LONG_MAX);
+	printf("printf: |%#l1.5x|\n", "");
+	ft_printf("printf: |%#l1.5x|\n", "");
+	printf("printf: |%#1.5p|\n", "");
+	ft_printf("printf: |%#1.5p|\n", "");
+	printf("%-#8.X\n", 42);
+	ft_printf("%-#8.X\n", 42);
+	printf("%#6o\n", 2500);
+	ft_printf("%#6o\n", 2500);
+	printf("%#08X\n", 42);
+	ft_printf("%#08X\n", 42);
+	printf("@moulitest: %5.x %5.0x|\n", 0, 0);
+	ft_printf("@moulitest: %5.x %5.0x|\n", 0, 0);
+	printf("%05.2s is a string\n", "");
+	ft_printf("%05.2s is a string\n", "");
+	printf("%lu\n", "-42");
+	ft_printf("%lu\n", "-42");
+	printf("%10s is a string\n", "");
+	ft_printf("%10s is a string\n", "");
+	printf("%zu, %zu\n", 0, ULLONG_MAX);
+	ft_printf("%zu, %zu\n", 0, ULLONG_MAX);
+	printf("|%s%9.5S%ls|\n", NULL, NULL, L"漢字");
+	ft_printf("|%s%9.5S%ls|\n", NULL, NULL, L"漢字");
+	printf("%.u, %.0u\n", 0, 0);
+	ft_printf("%.u, %.0u\n", 0, 0);
+	printf("{%-5p}\n", 0);
+	ft_printf("{%-5p}\n", 0);
+	printf("%p\n", 0);
+	ft_printf("%p\n", 0);
+	printf("% p|%+p\n", 42, 42);
+	ft_printf("% p|%+p\n", 42, 42);
+	printf("%.p, %.0p\n", 0, 0);
+	ft_printf("%.p, %.0p\n", 0, 0);
+	printf("{% 03d}\n", 0);
+	ft_printf("{% 03d}\n", 0);
+	printf("{%#6.3x}\n", 1);
+	ft_printf("{%#6.3x}\n", 1);
+	printf("%|#1.5x|\n", NULL);
+	ft_printf("%|#1.5x|\n", NULL);
+	printf("|%C|\n", L'�');
+	ft_printf("|%C|\n", L'�');
+	printf("% ");
+	ft_printf("% ");
+	printf("{%.*d}\n", -5, 42);
+	ft_printf("{%.*d}\n", -5, 42);
+	printf("{%*3d}\n", 0, 0);
+	ft_printf("{%*3d}\n", 0, 0);
+	printf("{%3*d}\n", 0, 0);
+	ft_printf("{%3*d}\n", 0, 0);
+	printf("{%.*s}\n", -5, "42");
+	ft_printf("{%.*s}\n", -5, "42");
+	printf("{%#x}\n", 42544554);
+	ft_printf("{%#x}\n", 42544554);
+	// printf("%-+ 5.9f|\n", 1.2234454556);
+	// ft_printf("%f ", );
+	*/
 	return (0);
 }
--- src/wchar.c
+++ src/wchar.c
@@ -6,7 +6,7 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/11 15:23:28 by blee              #+#    #+#             */
-/*   Updated: 2017/07/11 19:58:50 by blee             ###   ########.fr       */
+/*   Updated: 2017/07/12 18:58:17 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,59 @@
 
 char	*one_byte_str(wchar_t wc)
 {
+	char	*str;
+
+	str = ft_strnew(1);
+	*str = (unsigned char)wc;
+	return (str);
 }
 
 char	*two_byte_str(wchar_t wc)
 {
+	char	*str;
+
+	str = ft_strnew(2);
+	str[0] = (unsigned char)wc >> 6;
+	str[0] = str[0] | 0xC0;
+	str[1] = (unsigned char)wc & 0x3F;
+	str[1] += 0x80;
+	return (str);
 }
 
 char	*three_byte_str(wchar_t wc)
 {
+	char	*str;
+
+	str = ft_strnew(3);
+	str[0] = (unsigned char)wc >> 12;
+	str[0] = str[0] | 0xE0;
+	str[1] = (unsigned char)wc >> 6;
+	str[1] = (str[1] & 0x3F) + 0x80;
+	str[2] = (unsigned char)wc & 0x3F;
+	str[2] += 0x80;
+	return (str);
 }
 
 char	*four_byte_str(wchar_t wc)
 {
+	char	*str;
+
+	str = ft_strnew(4);
+	str[0] = (unsigned char)wc >> 18;
+	str[0] += 
 }
 
 char	*wchar_to_str(wchar_t wc)
 {
 	/* MB_CUR_MAX */
+	char	*out;
+	if (wc <= 0x7F)
+		out = one_byte_str(wc);
+	else if (wc <= 0x7FF)
+		out = two_byte_str(wc);
+	else if (wc <= 0xFFFF)
+		out = three_byte_str(wc);
+	else if (wc <= 0x10FFFF)
+		out = four_byte_str(wc);
+
 }
